8f44091c9d121bb89e2ba5e2ac534dfb71e9da0f
dineshkumarbarupal <dineshkumar5apd@gmail.com>
2024-09-07 13:26:35 +0530

    for loop practice

---
+++ for_loop_practice.py
@@ -0,0 +1,123 @@
+# for i in range(1,11):
+#     for a in range(1,11):
+#         print(f"{i}x{a} = {a * i}", end="  ")
+#     print()
+
+
+
+
+# for i in range(1,11):
+#     if i == 5:
+#         break
+#     else:
+#         print("this is a wrong input")
+#     print(i)
+
+# my_list = [13,23,98,37,123,32,43,23,34,34]
+# for itom in my_list:
+#     print(itom)
+# print(my_list[3])
+
+
+
+
+
+# my_list = [12,54,67,76,74,364,891]
+# for element in my_list:
+#     print(element)
+
+# new_list = my_list + [253,67, 478]
+# print(my_list)
+# print(new_list)
+
+# car_list = ["honda","alto","dezire","marcidies benz","rollce royce"]
+
+# for cars in car_list:
+#     if cars ==  "dezire":
+#         break
+#     print(cars)
+
+# slicing = car_list[1:4]
+# print(slicing)
+
+
+
+
+# my_dict = {
+#     "name": "dinesh",
+#     "age": 19,
+#     "mobile num.": 637781395
+# } 
+
+# for data in my_dict:
+#     print(data) 
+
+# print(my_dict)
+# name = my_dict["name"]
+# print(name)
+
+# for velue in my_dict.values():
+#     print(velue)
+
+# for key,velue in my_dict.items():
+#     print(f"{key},{velue}")
+
+
+
+
+# my_list = [12,34,523,634,678,232]
+# for itoms in my_list:
+#     print(itoms)
+
+
+# import numpy as np
+# my_array = np.array([1, 2, 3, 4])  # NumPy array for numerical operations
+
+
+# for i in range(1,230,10):
+#     print(i)
+
+
+
+
+# import numpy as np
+# my_array = np.array([12,234,47,78,8543,45324,123,112])
+# print(my_array)
+
+
+
+# my_list = [12,23,48,54,56]
+# my_list.append(123)
+# # my_list.append([234,123,3243,4353,345])
+# print(my_list)
+
+# my_list =  my_list + [12,213,234,435,647,]
+# print(my_list)
+
+
+
+my_list = [23,45,67,89,100,]
+for data in my_list:
+    print(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
