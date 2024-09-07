62b8e374341baac37d590ab84f0184fe4317c2ea
dineshkumarbarupal <dineshkuma5apd@gmail.com>
2024-09-07 21:08:30 +0530

    array practice

---
+++ 40_array_practice.py
@@ -0,0 +1,40 @@
+# import array as arr
+# arr = (23,45,6,67,5,4)
+# print(arr)
+
+
+# from array import *
+
+# vars = array("i",[23,3])
+# print(vars)
+
+
+
+# from array import *
+# var = array("i",[456, 56,-8,33,553, 53453])
+# print(var.buffer_info())
+
+            # this is a count mathod
+# from array import *
+# var1 = array("i",[12,3,23,23,3,3,3])
+# var2 = array("i",[2,34,3,2,34,23,23,2,2])
+# print(var2.count(2))
+# print(var1.count(3))
+
+
+from array import *
+number = array("i",[23,234,34,234,234])
+print(number)                   # output = array("i",[23,234,34,234,234])
+print(number.buffer_info())     # output = (2636686598320, 5)
+
+
+
+
+
+
+
+
+
+
+
+
