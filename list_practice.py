218f347bc867ddf8feef273be86c00e55ca55a76
dineshkumarbarupal <dineshkuma5apd@gmail.com>
2024-09-13 22:42:15 +0530

    list pracice

--- list_practice.py
+++ list_practice.py
@@ -5,8 +5,30 @@
 
 # Basic List Creation and Accessing Elements
 
-my_list = [1,2,3,4,5]
-print(my_list[0])        # output = 1
+# my_list = [1,2,3,4,5]
+# print(my_list[0])        # output = 1
+
+
+
+# my_list = [1,2,3,4,5]
+# print(my_list[-1])         # output = 5
+
+
+# my_list = [1,2,3,4,5]
+# print(my_list[0:5])           # output = [1,2,3,4,5]
+
+
+
+my_list = [1,2,3,4,5,6]
+print(my_list[2:5])             # output = [3,4,5]
+
+
+
+
+
+
+
+
 
 
 
