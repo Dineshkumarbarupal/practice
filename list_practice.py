96669b6acd2cc08bc4c19004a60644337c59c583
dineshkumarbarupal <dineshkuma5apd@gmail.com>
2024-09-14 21:08:37 +0530

    new chang

--- list_practice.py
+++ list_practice.py
@@ -22,20 +22,51 @@
 # my_list = [1,2,3,4,5,6]
 # print(my_list[2:5])             # output = [3,4,5]
 
-my_list = [1,2,3]
-my_list.append(4)
-print(my_list)                    # output = [1,2,3,4]
+
+                    
+                # append() method
+
+# my_list = [1,2,3]
+# my_list.append(4)
+# print(my_list)                    # output = [1,2,3,4]
+
+# my_list = [1,2,3,5]
+# my_list.append(6)
+# print(my_list)
+
+
+                # insert() methods
 
 
 
+# my_list = [1,2,4,5]
+# my_list.insert(2,3)
+# print(my_list)                # output = [1,2,3,4,5]
 
 
 
+                #    extend() method
+
+# new_list = ["a",6]
+# new_list.extend(my_list)
+# print(new_list)
 
 
+my_list = [1,2,3,4,5,3]
+my_list.remove(3)
+print(my_list)                 # output =  [1,2,4,5,3]
 
 
+my_list.pop()
+print(my_list)           # output =  [1,2,4,5]
 
+
+my_list.pop(1)
+print(my_list)           # output =  [1,4,5]
+
+
+my_list.clear()
+print(my_list)
 
 
 
