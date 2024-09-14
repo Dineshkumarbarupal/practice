97303791ddf54ed8bcc115b710e8a5e376a9523e
dineshkumarbarupal <dineshkumar5apd@gmail.com>
2024-09-14 10:59:50 +0530

    list practice

--- list_practice.py
+++ list_practice.py
@@ -22,9 +22,94 @@
 # my_list = [1,2,3,4,5,6]
 # print(my_list[2:5])             # output = [3,4,5]
 
-my_list = [1,2,3]
-my_list.append(4)
-print(my_list)                    # output = [1,2,3,4]
+# my_list = [1,2,3]
+# my_list.append(4)
+# print(my_list)                    # output = [1,2,3,4]
+
+
+                        #   clear() method
+# my_list = [1,2,3,4,5]
+# my_list.clear()
+# print(my_list)
+
+
+
+                         # pop method
+
+# my_list = [1,2,3,4,5]
+# my_list.pop(1)
+# print(my_list)
+
+
+                    #   insert() method
+
+# my_list = [1,2,4]
+# my_list.insert(2,3)
+# print(my_list)
+
+                          
+
+                    #  extend() methods    add multipale elements in list
+
+
+# my_list = [1,2,3,54]
+# my_list.extend([2,3,34,5,6,7,4])
+# print(my_list)                             #   [1,2,3,54,2,3,34,5,6,7,4]
+
+
+
+#                              # index() method  
+# my_list = [1,2,3,4,5]
+# print(my_list.index(4))             # output = 3
+
+                          
+
+
+                               #  sort() method       
+
+# my_list =  [1,2,3,4,34,56,73,5]
+# my_list.sort()
+# print(my_list)
+                              
+
+
+#                                    # reverse() method  
+# my_list = [1,2,3,4,5,6,7]
+# my_list.reverse()
+# print(my_list)
+
+
+
+                               # copy() method
+
+
+# my_list = [1,2,3,4,5,6]
+# new_list = my_list.copy()
+# print(new_list)
+
+
+                            #  count()  method 
+
+# new_list = [1,2,3,2,4,2,6,2,]
+# print(new_list.count(2))           # output = 4
+
+
+
+
+# new_list = [1,2,3,4,5,6]
+# print(new_list.count(2))          # output = 1
+
+
+
+
+new_list = [1,2,3,4,5]
+new_list.extend([1,2,3,4])
+print(new_list)
+
+
+
+
+
 
 
 
