09c2db657824e4eb187dc9c9c2f75da56c765189
dineshkumarbarupal <dineshkumar5apd@gmail.com>
2024-09-15 12:50:45 +0530

    new practice

--- 04_tuple_practice.py
+++ 04_tuple_practice.py
@@ -4,9 +4,54 @@
           #   creating tuple 
 
 
-my_tuple = (1,2,3,4)
-print(my_tuple)
-print(type(my_tuple))
+# my_tuple = (1,2,3,4)
+# print(my_tuple)         # output = (1,2,3,4)
+# print(type(my_tuple))   # output = <class 'Tuple'>
+
+  
+
+
+        # one value in tuple
+
+# my_tuple = (5)
+# print(my_tuple)
+# print(type(my_tuple))    # output = <class 'int'>
+
+
+# my_tuple = (5,)
+# print(my_tuple)   # output = (5,)
+# print(type(my_tuple))
+
+
+
+       # count() method in tuple
+# my_tuple = (1,2,3,2,5,2,7)
+# print(my_tuple.count(2))     # output = 3
+
+
+
+# my_tuple = (1,2,3,4,5)
+# print(my_tuple.index(3))     # output = 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
