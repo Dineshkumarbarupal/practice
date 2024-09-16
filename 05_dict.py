706fb6b3e6c10ce9272255cff7ddcec55f93bc8f
dineshkumarbarupal <dineshkumar5apd@gmail.com>
2024-09-16 16:25:50 +0530

    new changes

--- 05_dict.py
+++ 05_dict.py
@@ -51,7 +51,7 @@
 # student.pop("work")
 # print(student)
 
-# # removing itoms
+# # removing last itoms
 
 # student.popitem()
 # print(student)
@@ -100,7 +100,7 @@
 
 
 
-# Nested DictionariesNested Dictionaries
+# Nested Dictionaries Nested Dictionaries
 
 # students = {
 #     "student1":{
@@ -117,6 +117,122 @@
 # }
 
 # print(students["student1"],["student2"])
+
+
+
+# Dictionary Comprehension
+
+
+# squres = {x: x**2 for x in range(1,6)}
+
+# print(squres)
+
+
+
+# Merging Two Dictionaries
+
+
+# dict1 = {
+#     "name": "dinesh",
+#     "age": 19
+# }
+
+# dict2 = {
+#     "job": "Ai",
+#     "work_place": "alien company"
+# }
+
+
+# merging = dict1|dict2
+# print(merging)
+
+
+
+
+# Copying a Dictionary
+
+# original_dict = {
+#     "citi": "Srivijaynagar",
+#     "state": "Rajasthan"
+# }
+
+# copid_dict = original_dict.copy()
+
+# copid_dict["citi"] = "sriganganagar"
+
+# print(original_dict)
+# print(copid_dict)
+
+
+
+#  Default Values Using setdefault()
+
+# my_dict = {"name":"dinesh"}
+# age = my_dict.setdefault("age",19)
+# print(my_dict)
+
+
+# Counting with a Dictionary
+
+# word = "dictionary"
+# count_leter = {}
+
+# for letter in word:
+#     count_leter[letter] = count_leter.get(letter, 0) +1
+
+# print(count_leter) 
+
+
+# Sorting a Dictionary
+# my_dict = {
+#     "name": "raj",
+#     "age": 19,
+#     "job": "Ai"
+# }
+
+# sorted by key
+
+# sorted_by_key = dict(sorted(my_dict.items()))
+# print(sorted_by_key)
+
+# sorted by value
+
+# sorted_by_key = dict(sorted(my_dict.items(), key= lambda item:item[1]))
+# print(sorted_by_key)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
